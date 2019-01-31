016ae61c37d66bfe75ab6dc38802e2b6199e0581
Trevor Polischuk <trevorpolischuk@gmail.com>
2019-01-30 22:26:17 -0800

    Detecting if @ is pressed and some basic styling

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
 class Comments extends React.Component {
+
+  handleChange = (event) => {
+    if (event.key == "@") {
+      // Do Autocomplete Lookup
+    }
+  }
+
+  handleSubmit = () => {
+    //Handle Submit
+  }
+
+
   render() {
     return (
       <div>
         <h2 className="comments-title">Comments: </h2>
-        <textarea className="auto-complete comments">
+        <textarea className="auto-complete comments" onKeyDown={this.handleChange}>
         </textarea>
+        <button onClick={this.handleSubmit}>Submit</button>
       </div>
     )
   }
